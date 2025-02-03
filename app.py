c6c9cb6849ecf5daacb588742c146ca336930431
BRUCE-R-YOUNUG <boa2000viva@gmail.com>
2025-02-03 17:34:35 +0900

    update

--- app.py
+++ app.py
@@ -3,23 +3,17 @@ from datetime import datetime
 
 st.title("スナップ写真撮影アプリ")
 
-# カメラ入力ウィジェット
 image = st.camera_input("カメラで撮影してください")
 
-# 学科名、学籍番号、名前の入力欄
 dept = st.text_input("学科名")
 student_id = st.text_input("学籍番号")
 name = st.text_input("名前")
 
 if image is not None:
-    # すべての情報が入力されているかチェック
     if dept and student_id and name:
-        # タイムスタンプを利用してユニークなファイル名を生成
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        # ファイル名例: "情報工学_123456_山田太郎_20250203_101530.png"
         filename = f"{dept}_{student_id}_{name}_{timestamp}.png"
 
-        # 画像データの保存（image は BytesIO オブジェクト）
         with open(filename, "wb") as f:
             f.write(image.getvalue())
 
